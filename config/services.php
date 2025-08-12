2cc99ba51d8beae629e9a24697659c89c950e6b6
Mohamed0Taha <taha.elfatih@gmail.com>
2025-08-13 00:14:05 +0200

    OAuth + HTTPS/proxy fixes: Google Socialite, force HTTPS assets, trust proxies

--- config/services.php
+++ config/services.php
@@ -37,7 +37,7 @@ return [
     'google' => [
     'client_id'     => env('GOOGLE_CLIENT_ID'),
     'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-    'redirect'      => env('GOOGLE_REDIRECT_URI'),
+    'redirect'      => env('GOOGLE_REDIRECT_URI', 'https://laravel-react-automation-app-27e3cf659873.herokuapp.com/auth/google/callback'),
 ],
 
 
