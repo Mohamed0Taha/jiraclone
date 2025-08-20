701b47f985fc115ff347eb5c247f92f71f7830ae
Mohamed0Taha <taha.elfatih@gmail.com>
2025-08-20 19:14:52 +0200

    Fix email automation: remove queueable trait and set queue to sync

--- app/Mail/AutomationNotification.php
+++ app/Mail/AutomationNotification.php
@@ -2,13 +2,12 @@
 
 namespace App\Mail;
 
-use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
 class AutomationNotification extends Mailable
 {
-    use Queueable, SerializesModels;
+    use SerializesModels;
 
     public string $subjectLine;
     public string $contentText;
