441c4a94620793e9ab231e7e0b961824765829f0
Mohamed0Taha <taha.elfatih@gmail.com>
2025-08-24 02:51:33 +0200

    🧹 Remove build files from git tracking
    - Build files were temporarily added to ensure Heroku deployment
    - Heroku should build assets automatically via heroku-postbuild script
    - Removing tracked build files to keep repository clean

--- public/build/assets/Title-CM3NkCxE.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-BgEJ3fxA.js";import{c as t}from"./createSvgIcon-BJOZcqxU.js";const m=t(o.jsx("path",{d:"M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8zm2 16H8v-2h8zm0-4H8v-2h8zm-3-5V3.5L18.5 9z"})),r=t(o.jsx("path",{d:"M5 4v3h5.5v12h3V7H19V4z"}));export{m as D,r as T};
--- public/build/assets/ownerWindow-XbzhRCTS.js
+++
@@ -1,1 +0,0 @@
-import{o as n}from"./getReactElementRef-F5A8c8Er.js";function t(o){return n(o).defaultView||window}export{t as o};
