b4bfa9cdbd20dd6659b1f81e93d1d19f5839c7fe
Mohamed0Taha <taha.elfatih@gmail.com>
2025-09-03 05:10:16 +0200

    Fix CORS and asset URL configuration

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'build/*', 'appsumo/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'build/*', 'appsumo/*', 'assets/*'],
 
     'allowed_methods' => ['*'],
 
